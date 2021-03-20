2725f744ba42000545f44f45f6a61bf72f8fac91
ntuchkov <79327125+ntuchkov@users.noreply.github.com>
2021-03-20 13:17:42 +0300

    site

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function() {
+        $('.button__open').click(function(){
+                $(this).toggleClass('button__close');
+                $('.burger-menu').toggleClass('burger-menu__click');
+        })
+})
